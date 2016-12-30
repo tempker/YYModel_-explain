3a1fe137e62fa7ad77f63f3115779e01424ac840
WPF <weipengfei@panda.tv>
2016-12-30 14:56:42 +0800

    init

---
+++ YYModel_ explain/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YYModel_ explain
+//
+//  Created by WPF on 16/12/30.
+//  Copyright © 2016年 Tempker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YYModel_ explain/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YYModel_ explain
+//
+//  Created by WPF on 16/12/30.
+//  Copyright © 2016年 Tempker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
